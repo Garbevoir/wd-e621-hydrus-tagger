7eaf7b81a29e10be4b312bdc7a40cdae84c9aa3b
Garbevoir <141919206+Garbevoir@users.noreply.github.com>
2023-08-14 19:25:54 -0700

    Add files via upload

---
+++ ratings-hydrus-tagger/onnx_loader.py
@@ -0,0 +1,55 @@
+import importlib
+import sys
+import subprocess
+import os
+
+skip_install = False
+index_url = os.environ.get('INDEX_URL', "")
+python = sys.executable
+
+
+def is_installed(package):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        return False
+
+    return spec is not None
+
+
+def run_pip(args, desc=None):
+    if skip_install:
+        return
+
+    index_url_line = f' --index-url {index_url}' if index_url != '' else ''
+    return run(f'"{python}" -m pip {args} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}")
+
+
+def run(command, desc=None, errdesc=None, custom_env=None, live=False):
+    if desc is not None:
+        print(desc)
+
+    if live:
+        result = subprocess.run(
+            command, shell=True, env=os.environ if custom_env is None else custom_env)
+        if result.returncode != 0:
+            raise RuntimeError(f"""{errdesc or 'Error running command'}.
+Command: {command}
+Error code: {result.returncode}""")
+
+        return ""
+
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            shell=True, env=os.environ if custom_env is None else custom_env)
+
+    if result.returncode != 0:
+
+        message = f"""{errdesc or 'Error running command'}.
+Command: {command}
+Error code: {result.returncode}
+stdout: {result.stdout.decode(encoding="utf8", errors="ignore") if len(result.stdout)>0 else '<empty>'}
+stderr: {result.stderr.decode(encoding="utf8", errors="ignore") if len(result.stderr)>0 else '<empty>'}
+"""
+        raise RuntimeError(message)
+
+    return result.stdout.decode(encoding="utf8", errors="ignore")
---
+++ wd-hydrus-tagger/__main__.py
@@ -0,0 +1,123 @@
+import os.path
+
+import click
+from PIL import Image, ImageFile
+from . import interrogate
+import hydrus_api
+from io import BytesIO
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument("filename")
+@click.option("--cpu", default=False, help="Use CPU instead of GPU")
+@click.option("--model", default="SmilingWolf/wd-v1-4-vit-tagger-v2", help="The tagging model version to use")
+@click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
+def evaluate(filename, cpu, model, threshold):
+    integerator = interrogate.WaifuDiffusionInterrogator(
+        'wd14-vit-v2',
+        repo_id=model,
+        revision='v2.0'
+    )
+    integerator.load(cpu)
+    image = Image.open(filename)
+    ratings, tags = integerator.interrogate(image)
+    rating = "general"
+    for key in ratings.keys():
+        if ratings[key] > ratings[rating]:
+            rating = key
+    clipped_tags = []
+    for key in tags.keys():
+        if (tags[key] > threshold):
+            clipped_tags.append(key)
+    click.echo("rating: " + rating)
+    click.echo("tags: " + ",".join(clipped_tags))
+
+
+@click.command()
+@click.argument("hash")
+@click.option("--token", help="The API token for your Hydrus server")
+@click.option("--cpu", default=False, help="Use CPU instead of GPU")
+@click.option("--model", default="SmilingWolf/wd-v1-4-vit-tagger-v2", help="The tagging model version to use")
+@click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
+@click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
+def evaluate_api(hash, token, cpu, model, threshold, host):
+    integerator = interrogate.WaifuDiffusionInterrogator(
+        'wd14-vit-v2',
+        repo_id=model,
+        revision='v2.0'
+    )
+    integerator.load(cpu)
+    client = hydrus_api.Client(token, host)
+    image_bytes = BytesIO(client.get_file(hash).content)
+    image = Image.open(image_bytes)
+    ratings, tags = integerator.interrogate(image)
+    rating = "general"
+    for key in ratings.keys():
+        if ratings[key] > ratings[rating]:
+            rating = key
+    clipped_tags = []
+    for key in tags.keys():
+        if (tags[key] > threshold):
+            clipped_tags.append(key.replace("_", " "))
+    click.echo("rating: " + rating)
+    click.echo("tags: " + ",".join(clipped_tags))
+    clipped_tags.append("rating:" + rating)
+    client.add_tags(hashes=[hash], service_names_to_tags={
+        "my tags": clipped_tags
+    })
+
+
+@click.command()
+@click.argument("hashfile")
+@click.option("--token", help="The API token for your Hydrus server")
+@click.option("--cpu", default=False, help="Use CPU instead of GPU")
+@click.option("--model", default="SmilingWolf/wd-v1-4-vit-tagger-v2", help="The tagging model version to use")
+@click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
+@click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
+@click.option("--tag-service", default="my tags", help="The Hydrus tag service to add tags to")
+def evaluate_api_batch(hashfile, token, cpu, model, threshold, host, tag_service):
+    if not os.path.isfile(hashfile):
+        raise ValueError("hashfile not found")
+    integerator = interrogate.WaifuDiffusionInterrogator(
+        'wd14-vit-v2',
+        repo_id=model,
+        revision='v2.0'
+    )
+    integerator.load(cpu)
+    client = hydrus_api.Client(token, host)
+    with open(hashfile) as hashfile_f:
+        hashes = hashfile_f.readlines()
+
+    with click.progressbar(hashes) as bar:
+        for hash in bar:
+            click.echo(" processing: "+ hash)
+            image_bytes = BytesIO(client.get_file(hash).content)
+            image = Image.open(image_bytes)
+            ratings, tags = integerator.interrogate(image)
+            rating = "general"
+            for key in ratings.keys():
+                if ratings[key] > ratings[rating]:
+                    rating = key
+            clipped_tags = []
+            for key in tags.keys():
+                if (tags[key] > threshold):
+                    clipped_tags.append(key.replace("_", " "))
+            clipped_tags.append("rating:" + rating)
+            clipped_tags.append("wd-hydrus-tagger ai generated tags")
+            client.add_tags(hashes=[hash], service_names_to_tags={
+                tag_service: clipped_tags
+            })
+
+
+if __name__ == '__main__':
+    Image.init()
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    cli.add_command(evaluate)
+    cli.add_command(evaluate_api)
+    cli.add_command(evaluate_api_batch)
+    cli()
