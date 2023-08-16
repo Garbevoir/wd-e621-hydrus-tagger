9fe921554e30c9f541d4067b1e333f13453ec6de
Garbevoir <141919206+Garbevoir@users.noreply.github.com>
2023-08-15 18:29:46 -0700

    Update ratings.bat

--- ratings.bat
+++ ratings.bat
@@ -1,1 +1,1 @@
-python -m wd-hydrus-tagger evaluate-api-batch --token 4486487223a6ca8bbe3f399e5b97725c3bfc204e97935717fee47e767769cd1a --tag-service "A.I. Tags" --cpu 1 --ratings-only 1 hashes.txt
+python -m wd-hydrus-tagger evaluate-api-batch --token REPLACE_WITH_API_KEY --tag-service "A.I. Tags" --cpu 1 --ratings-only 1 hashes.txt
