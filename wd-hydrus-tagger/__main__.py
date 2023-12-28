dc1c2ce85be0b63e8e5ed1fdb4c4a3921e0183bf
Garbevoir <141919206+Garbevoir@users.noreply.github.com>
2023-12-27 23:00:23 -0800

    add --privacy flag for cli output
    --privacy flag controls whether or not the cli outputs the generated tags after a hash is processed. Works for both single hashes as well as bulk hashes. Default is true for both (hidden output).

--- wd-hydrus-tagger/__main__.py
+++ wd-hydrus-tagger/__main__.py
@@ -58,7 +58,7 @@ def evaluate(filename, cpu, model, threshold):
         if (tags[key] > threshold):
             clipped_tags.append(key)
     click.echo("rating: " + rating)
-    click.echo("tags: " + ",".join(clipped_tags))
+    click.echo("tags: " + ", ".join(clipped_tags))
 
 
 @click.command()
@@ -70,7 +70,8 @@ def evaluate(filename, cpu, model, threshold):
 @click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
 @click.option("--tag-service", default="my tags", help="The Hydrus tag service to add tags to")
 @click.option("--ratings-only", default=False, help="Strip all tags except for content rating")
-def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_only):
+@click.option("--privacy", default=True, help="hides the tag output from the cli")
+def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_only, privacy):
     integerator = interrogate.WaifuDiffusionInterrogator(
         'wd14-vit-v2',
         repo_id=model,
@@ -90,8 +91,9 @@ def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_
         for key in tags.keys():
             if (tags[key] > threshold):
                 clipped_tags.append(key.replace("_", " ") if key not in kaomojis else key)
-    click.echo("rating: " + rating)
-    click.echo("tags: " + ",".join(clipped_tags))
+        if not privacy:
+            click.echo("rating: " + rating)
+            click.echo("tags: " + ", ".join(clipped_tags))
     clipped_tags.append("rating:" + rating)
     if ratings_only:
         clipped_tags.append("ratings-hydrus-tagger ai generated tags")
@@ -111,7 +113,8 @@ def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_
 @click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
 @click.option("--tag-service", default="my tags", help="The Hydrus tag service to add tags to")
 @click.option("--ratings-only", default=False, help="Strip all tags except for content rating")
-def evaluate_api_batch(hashfile, token, cpu, model, threshold, host, tag_service, ratings_only):
+@click.option("--privacy", default=True, help="hides the tag output from the cli")
+def evaluate_api_batch(hashfile, token, cpu, model, threshold, host, tag_service, ratings_only, privacy):
     if not os.path.isfile(hashfile):
         raise ValueError("hashfile not found")
     integerator = interrogate.WaifuDiffusionInterrogator(
@@ -139,6 +142,10 @@ def evaluate_api_batch(hashfile, token, cpu, model, threshold, host, tag_service
                 for key in tags.keys():
                     if (tags[key] > threshold):
                         clipped_tags.append(key.replace("_", " ") if key not in kaomojis else key)
+            if not privacy:
+                click.echo("rating: " + rating)
+                click.echo("tags: " + ", ".join(clipped_tags))
+                click.echo()
             clipped_tags.append("rating:" + rating)
             if ratings_only:
                 clipped_tags.append("ratings-hydrus-tagger ai generated tags")
