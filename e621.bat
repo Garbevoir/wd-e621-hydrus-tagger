8c16de75ed9bfe84f2055838da58196e595e18ec
Garbevoir <141919206+Garbevoir@users.noreply.github.com>
2023-12-30 00:41:27 -0800

    Update e621.bat to point to specific folder

--- e621.bat
+++ e621.bat
@@ -1,1 +1,1 @@
-python -m e621-hydrus-tagger evaluate-api-batch --token REPLACE_WITH_API_KEY --tag-service "A.I. Tags" --cpu 1 hashes.txt
+python -m wd-hydrus-tagger evaluate-api-batch --token REPLACE_WITH_API_KEY --tag-service "A.I. Tags" --cpu 1 --model Z3D-E621-Convnext hashes.txt
